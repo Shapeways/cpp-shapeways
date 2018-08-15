73a3226f0b644c75db2e3a7ee3d4a6625d2627ce
matt <matt@shapeways.com>
2018-08-15 09:56:31 -0400

    Fix doc

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 
 int main(void)
 {
-  swApiClient * client = new swApiClient("CLIENT_KEY", "CLIENT_SECRET");
+  // Insert your Ouath2 Client ID and Client Secrets here.
+  // You can find them at https://developers.shapeways.com/manage-apps
+  swApiClient *client = new swApiClient("CLIENT_ID", "CLIENT_SECRET");
   client->Authorize();
-  client->GetMaterials();
+  // client->GetMaterials();
 }
